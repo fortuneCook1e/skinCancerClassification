863b230c055f8af010a26e8dc04ba463841aa63c
jeeshian <jeeshian2001@gmail.com>
2024-04-16 23:31:31 +0800

    change seg region to binary, include title for img, change contour and mask position

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def get_segment(model, image):
     original_image_vis = image.copy()
     # Convert image to BGR for OpenCV if needed
     original_image_vis_cv2 = (original_image_vis * 255).astype(np.uint8)
-    # original_image_vis_cv2 = cv2.cvtColor(original_image_vis_cv2, cv2.COLOR_RGB2BGR)
+    original_image_vis_cv2 = cv2.cvtColor(original_image_vis_cv2, cv2.COLOR_RGB2BGR)
     # Draw contours on the original image for OpenCV
     cv2.drawContours(original_image_vis_cv2, contours_pred, -1, (255, 0, 0), 3)  # Predicted mask in blue
     return original_image_vis_cv2, pred_mask
@@ -36,7 +36,7 @@ def get_segment(model, image):
 def preprocess_image(image_path, target_size=(192, 256)):
     image = cv2.imread(image_path)
     original_size = image.shape[:2]  # Capture original size before resizing
-    # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     image = cv2.resize(image, target_size) / 255.0  # Normalize to 0-1
     return image, original_size  # Return both processed image and original size
 
@@ -47,6 +47,7 @@ def resize_image_to_original(img_with_contour, original_size):
 
 def get_region(original_image, pred_mask):
     original_image = (original_image * 255).astype(np.uint8)
+    # original_image = cv2.cvtColor(original_image, cv2.COLOR_RGB2BGR)  # if open: 27
     # Use the binary mask to extract the segmented region
     segmented_region = cv2.bitwise_and(original_image, original_image, mask=pred_mask)
     return segmented_region
@@ -75,6 +76,28 @@ def classify_img(model, segmented_region):
 # Load your models here (assuming they are loaded only once to save resources)
 segmentation_model = load_model('C:/Users/jeesh/Documents(local)/FYP/fyp code/chosen_models/resnet50_segmentation-unfreeze.h5')
 classification_model = load_model('C:/Users/jeesh/Documents(local)/FYP/fyp code/chosen_models/AttentionInceptionV3(with_extraction)-r8_k3.h5')
+
+# Get a list of all image files in the source directory
+# source_directory = "C:/Users/jeesh/Documents(local)/FYP/test set/splitted_benign_malignant_testset(original)/1"
+
+# image_files = [f for f in os.listdir(source_directory) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+# counter = 0
+# # Loop through all images with a progress bar
+# for image_file in image_files:
+#     image_path = os.path.join(source_directory, image_file)
+#     preprocessed_image, original_size = preprocess_image(image_path)
+#     # Get the contour and mask
+#     img_with_contour, pred_mask = get_segment(segmentation_model, preprocessed_image)
+#     # Get tumour region
+#     segmented_region = get_region(preprocessed_image, pred_mask)
+
+#     # Perform classification on the segmented region
+#     predicted_class_label, confidence_score = classify_img(classification_model, segmented_region)
+#     if (predicted_class_label == 'Malignant'):
+#         print(image_file)
+#         counter = counter + 1
+# print("All images have been processed and saved.")
+# print("malignant count = ", counter)
 
 # # Ensure the upload and segmented folders exist
 # os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
@@ -126,19 +149,25 @@ def predict():
         
         # Resize the image with contour back to original size
         img_with_contour_resized = resize_image_to_original(img_with_contour, original_size)
-        segmented_region_resized = resize_image_to_original(segmented_region, original_size)
+        # segmented_region_resized = resize_image_to_original(segmented_region, original_size)
+        # display_segmented_region = cv2.cvtColor(segmented_region_resized, cv2.COLOR_RGB2BGR)
+        pred_mask_resized = resize_image_to_original(pred_mask, original_size)
 
         # Encode the segmented image to base64 to send in the JSON response
         _, buffer = cv2.imencode('.png', img_with_contour_resized)
         b64_segmented_image = base64.b64encode(buffer).decode('utf-8')
         
-        _, buffer2 = cv2.imencode('.png', segmented_region_resized)
-        b64_mask_image = base64.b64encode(buffer2).decode('utf-8')
+        # _, buffer2 = cv2.imencode('.png', display_segmented_region)
+        # b64_mask_image = base64.b64encode(buffer2).decode('utf-8')
+        
+         # Encode pred_mask to base64
+        _, buffer3 = cv2.imencode('.png', pred_mask_resized * 255)  # Convert binary mask to a visible format
+        b64_mask = base64.b64encode(buffer3).decode('utf-8')
 
         # Return the classification result and the base64-encoded image
         return jsonify({
             'segmented_image': f"data:image/png;base64,{b64_segmented_image}",
-            'mask_image': f"data:image/png;base64,{b64_mask_image}",
+            'mask_image': f"data:image/png;base64,{b64_mask}",
             'predicted_class': predicted_class_label,
             'confidence_score': f"{confidence_score:.2f}"
         })
